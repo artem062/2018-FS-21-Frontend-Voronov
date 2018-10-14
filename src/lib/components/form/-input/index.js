e7c121fb2444dd530bcfa7e435915fb989529fa6
artem062 <artem_062@mail.ru>
2018-10-14 17:43:05 +0300

    HW2_2

--- src/lib/components/form/-input/index.js
+++ src/lib/components/form/-input/index.js
@@ -1,55 +1,53 @@
-import styles from './index.css';
+/* eslint no-underscore-dangle: 0 */
+/* eslint-env browser */
+// import styles from './index.css';
 import shadowStyles from './shadow.css';
 
 const template = `
-	<style>${shadowStyles.toString()}</style>
-	<input />
-	<slot name="icon"></slot>
+    <style>${shadowStyles.toString()}</style>
+    <input />
+    <slot name="icon"></slot>
 `;
 
-//const iconTemplate = `
-//	<div class="${styles.icon}" />
-//`;
-
 class FormInput extends HTMLElement {
-	constructor () {
-		super();
-		const shadowRoot = this.attachShadow({mode: 'open'});
-		shadowRoot.innerHTML = template;
-		this._initElements();
-		this._addHandlers();
-	}
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.innerHTML = template;
+    this._initElements();
+    this._addHandlers();
+  }
 
-	static get observedAttributes() {
-		return [
-			"name",
-			"placeholder",
-			"value",
-			"disabled"
-		]
-	}
+  static get observedAttributes() {
+    return [
+      'name',
+      'placeholder',
+      'value',
+      'disabled',
+    ];
+  }
 
-	attributeChangedCallback(attrName, oldVal, newVal) {
-		this._elements.input[attrName] = newVal;
-	}
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    this.elements.input[attrName] = newVal;
+  }
 
-	_initElements () {
-		var hiddenInput = document.createElement('input');
-		var input = this.shadowRoot.querySelector('input');
-		this.appendChild(hiddenInput);
-		this._elements = {
-			input: input,
-			hiddenInput: hiddenInput
-		};
-	}
+  _initElements() {
+    const hiddenInput = document.createElement('input');
+    const input = this.shadowRoot.querySelector('input');
+    this.appendChild(hiddenInput);
+    this.elements = {
+      input,
+      hiddenInput,
+    };
+  }
 
-	_addHandlers () {
-		this._elements.input.addEventListener('input', this._onInput.bind(this));
-	}
+  _addHandlers() {
+    this.elements.input.addEventListener('input', this._onInput.bind(this));
+  }
 
-	_onInput () {
-		this._elements.hiddenInput.value = this._elements.input.value;
-	}
+  _onInput() {
+    this.elements.hiddenInput.value = this.elements.input.value;
+  }
 }
 
 customElements.define('form-input', FormInput);
