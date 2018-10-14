40625fc73b2b4db7e2c1a5d32d21a398d6bc6147
artem062 <36705486+artem062@users.noreply.github.com>
2018-10-14 17:54:27 +0300

    Delete test.js

--- src/lib/test.js
+++
@@ -1,7 +0,0 @@
-const say = function (name) {
-    var div = document.createElement('div');
-    div.innerHTML = '<h1>Hello, ' + name + '!</h1>';
-    document.body.appendChild(div);
-}
-
-export default say;
